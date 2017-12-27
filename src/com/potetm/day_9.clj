9169003d24ccddbb617120e0fa9e347ceb95479b
Tim Pote <pote.timothy@gmail.com>
2017-12-26 20:47:30 -0600

    day9

---
+++ src/com/potetm/day_9.clj
@@ -0,0 +1,69 @@
+(ns com.potetm.day-9
+  (:require [clojure.string :as str]))
+
+(defn parse [s]
+  ;; NEVER do this in real life
+  (seq s))
+
+(defn remove-garbage [s]
+  (let [drp-gbg (fn [s]
+                  (loop [s s]
+                    (if (seq s)
+                      (case (first s)
+                        \! (recur (drop 2 s))
+                        \> (next s)
+                        (recur (next s)))
+                      s)))]
+    (loop [s s
+           out []]
+      (if (seq s)
+        (let [c (first s)]
+          (if (= c \<)
+            (recur (drp-gbg s)
+                   out)
+            (recur (next s)
+                   (conj out c))))
+        out))))
+
+(defn count-garbage [s]
+  (let [cnt-gbg (fn [s]
+                  (loop [s s
+                         c 0]
+                    (if (seq s)
+                      (case (first s)
+                        \! (recur (drop 2 s)
+                                  c)
+                        \> [(next s) c]
+                        (recur (next s)
+                               (inc c)))
+                      s)))]
+    (loop [s s
+           cnt 0]
+      (if (seq s)
+        (let [c (first s)]
+          (if (= c \<)
+            (let [[s cg] (cnt-gbg (next s))]
+              (recur s
+                     (+ cg cnt)))
+            (recur (next s)
+                   cnt)))
+        cnt))))
+
+(defn countem [s]
+  (loop [mult 0
+         s s
+         c 0]
+    (if (seq s)
+      (case (first s)
+        \{ (recur (inc mult)
+                  (next s)
+                  c)
+        \} (recur (dec mult)
+                  (next s)
+                  (+ c mult))
+        \, (recur mult
+                  (next s)
+                  c))
+      c)))
+
+(countem (remove-garbage (parse "{{<a!>},{<a!>},{<a!>},{<ab>}}")))
